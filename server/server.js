5dbeb40cff03d6a3addf8a92472a1271a475e582
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-26 17:01:06 +0530

    Add initial start to server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');  
+const port = 3000; 
+
+const app = express();
+
+app.listen(port, (err) => {
+    if (!err) {
+        console.log(`Server is running on port ${port}`);
+    }
+})
