a6664c151929379480582779cab530e37f20a807
savioshaju <savioshajum@gmail.com>
2025-09-26 21:14:39 +0530

    footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default function Footer() {
-  return (
-    <div>Footer 1</div>
-  )
+    return (
+        <div className="absolute bottom-0 left-0 w-full bg-black text-white px-6 py-4 z-10 text-center text-sm">
+            <span className="ml-2 text-xs text-gray-300">
+                <div
+                    className="font-signature hover:text-blue-300"
+                >
+                    © 2025 CTRL_ALT_DFEAT. All rights reserved.
+                </div>
+            </span>
+        </div>
+    )
 }
