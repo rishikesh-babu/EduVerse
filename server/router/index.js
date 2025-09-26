8513e0c5b9d16f87971a24508cc85381b04d7e89
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-26 18:16:46 +0530

    Add backend routes

--- server/router/index.js
+++ server/router/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
+// router.use('/user')
 
 const apiRouter = router
 module.exports = apiRouter;
---
+++ server/router/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// router.post('/login'); 
+
+const userRoutes = router
+module.exports = userRoutes;
