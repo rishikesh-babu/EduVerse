105cbcc442d88c40e2b77e47c3b2bd2d8562787f
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-26 17:44:48 +0530

    delte node modules

---
+++ server/router/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+const apiRouter = router
+module.exports = apiRouter;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');  
+const apiRouter = require('./router');
 const port = 3000; 
 
 const app = express();
@@ -10,3 +11,5 @@ app.listen(port, (err) => {
         console.log(`Server is running on port ${port}`);
     }
 })
+
+app.use('/api', apiRouter);
