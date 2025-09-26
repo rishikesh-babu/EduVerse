1eea816e81b4ad3051a929d1d7cb3aff7c8f4559
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-26 18:36:04 +0530

    Add navbar

--- client/src/Router/UserLayout.jsx
+++ client/src/Router/UserLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../Components/Navbar'
 
 export default function UserLayout() {
   return (
     <div>
-        header 
+        <Navbar />
         <Outlet />
         footer 
     </div>
