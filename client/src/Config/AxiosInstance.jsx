abfedf5c309a79d99af05ce5bd4eff6c0d1e2b71
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-26 20:56:49 +0530

    Add axios instance

---
+++ client/src/Config/AxiosInstance.jsx
@@ -0,0 +1,8 @@
+const { base } = require("daisyui/imports");
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
+})
+
+export default axiosInstance; 
