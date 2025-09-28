da2cb9834134ad9620e525c2beaaa179e8eef9fa
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-09-28 22:05:10 +0530

    Add PWA, install app

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,31 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'EduVerse',
+                short_name: 'EduVerse',
+                description: 'Free Education for All',
+                theme_color: '#6200ea',
+                background_color: '#ffffff',
+                display: 'standalone',
+                start_url: '/',
+                icons: [
+                    {
+                        src: 'Favicon.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'Favicon.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
 })
